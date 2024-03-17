5e27184aea9c2fb3369695c77c5be65493985d45
ethpsdu <ethan.stapff@gmail.com>
2024-03-17 11:46:35 +0100

    recursive Parentnode to_html + tests

--- src/HTMLNode.py
+++ src/HTMLNode.py
@@ -26,14 +26,32 @@ class LeafNode(HTMLNode):
             return self.value
         else:
             return f"<{self.tag}>{self.value}</{self.tag}>"
+# Parent of other Nodes, tree-like architecture
 class ParentNode(HTMLNode):
     def __init__(self, tag ,children, props=None):
         super().__init__(tag,None, children, props)
         self.children = children
+    # Recursive Function to read Tree of Nodes from Parentnode
     def to_html(self):
+        
         if self.tag is None:
             raise ValueError("Tag not provided")
         if self.children is None:
             raise ValueError("Children not provided")
         else:
-            pass
+            tagged_text = ""
+            parent_tag = self.tag
+            def recursive_stuff(current_node):
+                nonlocal tagged_text
+                nonlocal parent_tag
+                tagged_text += f"<{parent_tag}>"
+                for node in current_node:
+                    node_vals = node
+                    if node.children is None:
+                        tagged_text +=f"<{node.to_html()}"
+                    else:
+                        recursive_stuff(node_vals.children)
+                tagged_text += f"</{parent_tag}>"
+                return tagged_text
+                return recursive_stuff
+            return recursive_stuff(self.children)
--- src/test_HTMLNode.py
+++ src/test_HTMLNode.py
@@ -2,20 +2,34 @@ import unittest
 
 from HTMLNode import HTMLNode
 from HTMLNode import LeafNode
+from HTMLNode import ParentNode
 
 
 class TestHTMLNode(unittest.TestCase):
     def test_eq(self):
-        node = HTMLNode("This is a text node", "bold", None, {"href": "https://www.google.com", "target": "_blank"})
-        node2 = HTMLNode("This is a text node", "bold", None, {"href": "https://www.google.com", "target": "_blank"})
+        node = HTMLNode("l", "bold", "lll", {"href": "https://www.google.com", "target": "_blank"})
+        node2 = HTMLNode("b", "bold", "sample", {"href": "https://www.google.com", "target": "_blank"})
+        node3 = LeafNode("p", "adsafaa")
+        pr_node = ParentNode(
+            "p",
+            [
+                LeafNode("b", "Bold text"),
+                LeafNode(None, "Normal text"),
+                LeafNode("i", "italic text"),
+                LeafNode(None, "Normal text"),
+            ],
+        )
+
+        #node.to_html()
 
         print(HTMLNode.__repr__(node))
         print("efdsfdsf")
         print(HTMLNode.props_to_html(node2))
 
         node1 = LeafNode("p","ablrjpjmasmd",None, None)
-
-        print(LeafNode.to_html(node1))
+        print("------")
+        print(node1.to_html())
+        print(pr_node.to_html())
 
 if __name__ == "__main__":
     unittest.main()
